d72368b448a0a163b5493af25edbfa3ae56d9446
daviev <davidvalleau@gmail.com>
2022-08-23 23:36:32 -0700

    Adding button to reset/resize grid

--- script.js
+++ script.js
@@ -1,15 +1,34 @@
 let container = document.querySelector(".container");
+let resetButton = document.querySelector("#reset");
 
-let gridSize = 16;
-
-for (let i = 0; i < gridSize; ++i) {
-  let row = document.createElement("div");
-  row.classList.add("row");
-  for (let j = 0; j < gridSize; ++j) {
-    let cell = document.createElement("div");
-    cell.classList.add("cell");
-    cell.addEventListener("mouseover", () => cell.classList.add("black"));
-    row.appendChild(cell);
+resetButton.addEventListener("click", () => {
+  let gridSize = parseInt(prompt("Enter the new grid size"));
+  if (isNaN(gridSize)) {
+    return;
   }
-  container.appendChild(row);
+  clearGrid();
+  createGrid(gridSize);
+});
+
+function clearGrid() {
+  while (container.hasChildNodes()) {
+    container.removeChild(container.firstChild);
+  }
 }
+
+function createGrid(gridSize) {
+  console.log(gridSize);
+  for (let i = 0; i < gridSize; ++i) {
+    let row = document.createElement("div");
+    row.classList.add("row");
+    for (let j = 0; j < gridSize; ++j) {
+      let cell = document.createElement("div");
+      cell.classList.add("cell");
+      cell.addEventListener("mouseover", () => cell.classList.add("black"));
+      row.appendChild(cell);
+    }
+    container.appendChild(row);
+  }
+}
+
+createGrid(16);
